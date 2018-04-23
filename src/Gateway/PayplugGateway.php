d5893c4c6a9591a24b6787fcb9e95404d0218a29
petitphp <c.boirie@gmail.com>
2018-04-23 10:01:27 +0200

    coding standard

--- src/Gateway/PayplugGateway.php
+++ src/Gateway/PayplugGateway.php
@@ -261,59 +261,59 @@ class PayplugGateway extends WC_Payment_Gateway {
 
 		$payplug_requirements = new PayplugGatewayRequirements( $this ); ?>
 
-		<h2 class="title--logo"><?php esc_html( $this->get_method_title() ) ?></h2>
-		<p><?php _e( sprintf( 'Version %s', PAYPLUG_GATEWAY_VERSION ) ); ?></p>
-		<div class="payplug-requirements">
+        <h2 class="title--logo"><?php esc_html( $this->get_method_title() ) ?></h2>
+        <p><?php _e( sprintf( 'Version %s', PAYPLUG_GATEWAY_VERSION ) ); ?></p>
+        <div class="payplug-requirements">
 			<?php echo $payplug_requirements->curl_requirement(); ?>
 			<?php echo $payplug_requirements->php_requirement(); ?>
 			<?php echo $payplug_requirements->openssl_requirement(); ?>
 			<?php echo $payplug_requirements->account_requirement(); ?>
 			<?php echo $payplug_requirements->currency_requirement(); ?>
-		</div>
+        </div>
 		<?php echo wp_kses_post( wpautop( $this->get_method_description() ) ); ?>
 
 		<?php if ( $this->user_logged_in() ) : ?>
-			<table class="form-table">
+            <table class="form-table">
 				<?php $this->generate_settings_html( $this->get_form_fields() ); ?>
-			</table>
+            </table>
 		<?php else:
 			$GLOBALS['hide_save_button'] = true; ?>
-			<h3 class="wc-settings-sub-title"><?php _e( 'Connexion', 'payplug' ); ?></h3>
-			<table class="form-table">
-				<tbody>
-				<tr valign="top">
-					<th scope="row" class="titledesc">
-						<label for="payplug_email"><?php _e( 'Email', 'payplug' ); ?></label>
-					</th>
-					<td class="forminp">
-						<fieldset>
-							<legend class="screen-reader-text"><span><?php _e( 'Email', 'payplug' ); ?></span></legend>
-							<input class="input-text regular-input" type="text" name="payplug_email" id="payplug_email"
-							       value="" placeholder="<?php _e( 'your@email.com', 'payplug' ); ?>"/>
-						</fieldset>
-					</td>
-				</tr>
-				<tr valign="top">
-					<th scope="row" class="titledesc">
-						<label for="payplug_password"><?php _e( 'Password', 'payplug' ); ?></label>
-					</th>
-					<td class="forminp">
-						<fieldset>
-							<legend class="screen-reader-text"><span><?php _e( 'Password', 'payplug' ); ?></span>
-							</legend>
-							<input class="input-text regular-input" type="password" name="payplug_password"
-							       id="payplug_password" value=""/>
-						</fieldset>
-					</td>
-				</tr>
-				<tr valign="top">
-					<td class="forminp">
-						<input class="button" type="submit" value="<?php _e( 'Login', 'payplug' ); ?>">
+            <h3 class="wc-settings-sub-title"><?php _e( 'Connexion', 'payplug' ); ?></h3>
+            <table class="form-table">
+                <tbody>
+                <tr valign="top">
+                    <th scope="row" class="titledesc">
+                        <label for="payplug_email"><?php _e( 'Email', 'payplug' ); ?></label>
+                    </th>
+                    <td class="forminp">
+                        <fieldset>
+                            <legend class="screen-reader-text"><span><?php _e( 'Email', 'payplug' ); ?></span></legend>
+                            <input class="input-text regular-input" type="text" name="payplug_email" id="payplug_email"
+                                   value="" placeholder="<?php _e( 'your@email.com', 'payplug' ); ?>"/>
+                        </fieldset>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row" class="titledesc">
+                        <label for="payplug_password"><?php _e( 'Password', 'payplug' ); ?></label>
+                    </th>
+                    <td class="forminp">
+                        <fieldset>
+                            <legend class="screen-reader-text"><span><?php _e( 'Password', 'payplug' ); ?></span>
+                            </legend>
+                            <input class="input-text regular-input" type="password" name="payplug_password"
+                                   id="payplug_password" value=""/>
+                        </fieldset>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td class="forminp">
+                        <input class="button" type="submit" value="<?php _e( 'Login', 'payplug' ); ?>">
 						<?php wp_nonce_field( 'payplug_user_login', '_loginaction' ); ?>
-					</td>
-				</tr>
-				</tbody>
-			</table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
 		<?php
 		endif;
 	}
@@ -436,7 +436,7 @@ class PayplugGateway extends WC_Payment_Gateway {
 				'metadata'         => [
 					'order_id'    => $order_id,
 					'customer_id' => ( (int) $customer_id > 0 ) ? $customer_id : 'guest',
-					'domain' => $this->limit_length( $domain, 500 ),
+					'domain'      => $this->limit_length( $domain, 500 ),
 				]
 			] );
 
@@ -446,7 +446,7 @@ class PayplugGateway extends WC_Payment_Gateway {
 		}
 
 		return [
-			'result' => 'success',
+			'result'   => 'success',
 			'redirect' => $redirect_url,
 		];
 	}
@@ -539,10 +539,10 @@ class PayplugGateway extends WC_Payment_Gateway {
 
 		ob_start();
 		?>
-		<input
-				type="<?php echo esc_attr( $data['type'] ); ?>" name="<?php echo esc_attr( $field_key ); ?>"
-				id="<?php echo esc_attr( $field_key ); ?>"
-				value="<?php echo esc_attr( $this->get_option( $key ) ); ?>"/>
+        <input
+                type="<?php echo esc_attr( $data['type'] ); ?>" name="<?php echo esc_attr( $field_key ); ?>"
+                id="<?php echo esc_attr( $field_key ); ?>"
+                value="<?php echo esc_attr( $this->get_option( $key ) ); ?>"/>
 		<?php
 
 		return ob_get_clean();
@@ -578,43 +578,43 @@ class PayplugGateway extends WC_Payment_Gateway {
 
 		ob_start();
 		?>
-		<tr valign="top">
+        <tr valign="top">
 			<?php if ( ! $data['hide_label'] ) : ?>
-				<th scope="row" class="titledesc">
+                <th scope="row" class="titledesc">
 					<?php echo $this->get_tooltip_html( $data ); ?>
-					<label for="<?php echo esc_attr( $field_key ); ?>"><?php echo wp_kses_post( $data['title'] ); ?></label>
-				</th>
+                    <label for="<?php echo esc_attr( $field_key ); ?>"><?php echo wp_kses_post( $data['title'] ); ?></label>
+                </th>
 			<?php endif; ?>
-			<td class="forminp">
-				<fieldset>
-					<legend class="screen-reader-text"><span><?php echo wp_kses_post( $data['title'] ); ?></span>
-					</legend>
-					<div class="radio--custom">
-						<input class="radio radio-yes <?php echo esc_attr( $data['class'] ); ?>"
-						       type="radio"
-						       name="<?php echo esc_attr( $field_key ); ?>"
-						       id="<?php echo esc_attr( $field_key ); ?>-yes"
-						       value="1"
+            <td class="forminp">
+                <fieldset>
+                    <legend class="screen-reader-text"><span><?php echo wp_kses_post( $data['title'] ); ?></span>
+                    </legend>
+                    <div class="radio--custom">
+                        <input class="radio radio-yes <?php echo esc_attr( $data['class'] ); ?>"
+                               type="radio"
+                               name="<?php echo esc_attr( $field_key ); ?>"
+                               id="<?php echo esc_attr( $field_key ); ?>-yes"
+                               value="1"
 							<?php checked( '1', $checked ); ?>
 							<?php disabled( $data['disabled'], true ); ?>
 							<?php echo $this->get_custom_attribute_html( $data ); ?>>
-						<label for="<?php echo esc_attr( $field_key ); ?>-yes"><?php echo esc_html( $data['yes'] ); ?></label>
-					</div>
-					<div class="radio--custom">
-						<input class="radio radio-no <?php echo esc_attr( $data['class'] ); ?>"
-						       type="radio"
-						       name="<?php echo esc_attr( $field_key ); ?>"
-						       id="<?php echo esc_attr( $field_key ); ?>-no"
-						       value="0"
+                        <label for="<?php echo esc_attr( $field_key ); ?>-yes"><?php echo esc_html( $data['yes'] ); ?></label>
+                    </div>
+                    <div class="radio--custom">
+                        <input class="radio radio-no <?php echo esc_attr( $data['class'] ); ?>"
+                               type="radio"
+                               name="<?php echo esc_attr( $field_key ); ?>"
+                               id="<?php echo esc_attr( $field_key ); ?>-no"
+                               value="0"
 							<?php checked( '0', $checked ); ?>
 							<?php disabled( $data['disabled'], true ); ?>
 							<?php echo $this->get_custom_attribute_html( $data ); ?>>
-						<label for="<?php echo esc_attr( $field_key ); ?>-no"><?php echo esc_html( $data['no'] ); ?></label>
-					</div>
+                        <label for="<?php echo esc_attr( $field_key ); ?>-no"><?php echo esc_html( $data['no'] ); ?></label>
+                    </div>
 					<?php echo $this->get_description_html( $data ); ?>
-				</fieldset>
-			</td>
-		</tr>
+                </fieldset>
+            </td>
+        </tr>
 		<?php
 
 		return ob_get_clean();
@@ -647,29 +647,29 @@ class PayplugGateway extends WC_Payment_Gateway {
 
 		ob_start();
 		?>
-		<tr valign="top">
-			<th scope="row" class="titledesc">
+        <tr valign="top">
+            <th scope="row" class="titledesc">
 				<?php echo $this->get_tooltip_html( $data ); ?>
-				<label for="<?php echo esc_attr( $field_key ); ?>"><?php echo wp_kses_post( $data['title'] ); ?></label>
-			</th>
-			<td class="forminp">
-				<fieldset>
-					<legend class="screen-reader-text"><span><?php echo wp_kses_post( $data['title'] ); ?></span>
-					</legend>
+                <label for="<?php echo esc_attr( $field_key ); ?>"><?php echo wp_kses_post( $data['title'] ); ?></label>
+            </th>
+            <td class="forminp">
+                <fieldset>
+                    <legend class="screen-reader-text"><span><?php echo wp_kses_post( $data['title'] ); ?></span>
+                    </legend>
 					<?php foreach ( $data['options'] as $option_key => $option_value ) : ?>
-						<input class="radio <?php echo esc_attr( $data['class'] ); ?>"
-						       type="radio"
-						       name="<?php echo esc_attr( $field_key ); ?>"
-						       id="<?php echo esc_attr( $field_key ); ?>-<?php echo esc_attr( $option_key ); ?>"
-						       value="<?php echo esc_attr( $option_key ); ?>"
+                        <input class="radio <?php echo esc_attr( $data['class'] ); ?>"
+                               type="radio"
+                               name="<?php echo esc_attr( $field_key ); ?>"
+                               id="<?php echo esc_attr( $field_key ); ?>-<?php echo esc_attr( $option_key ); ?>"
+                               value="<?php echo esc_attr( $option_key ); ?>"
 							<?php checked( $option_key, $this->get_option( $key ) ); ?>
 							<?php disabled( $data['disabled'], true ); ?>
 							<?php echo $this->get_custom_attribute_html( $data ); ?>>
-						<label for="<?php echo esc_attr( $field_key ); ?>-<?php echo esc_attr( $option_key ); ?>"><?php echo esc_html( $option_value ); ?></label>
+                        <label for="<?php echo esc_attr( $field_key ); ?>-<?php echo esc_attr( $option_key ); ?>"><?php echo esc_html( $option_value ); ?></label>
 					<?php endforeach; ?>
-				</fieldset>
-			</td>
-		</tr>
+                </fieldset>
+            </td>
+        </tr>
 		<?php
 
 		return ob_get_clean();
@@ -691,17 +691,18 @@ class PayplugGateway extends WC_Payment_Gateway {
 
 		ob_start();
 		?>
-		<tr valign="top">
-			<td class="forminp">
-				<p><?php echo $this->get_option( 'email' ); ?></p>
-				<p>
-					<input type="submit" name="submit_logout" value="<?php _e( 'Logout', 'payplug' ); ?>">
+        <tr valign="top">
+            <td class="forminp">
+                <p><?php echo $this->get_option( 'email' ); ?></p>
+                <p>
+                    <input type="submit" name="submit_logout" value="<?php _e( 'Logout', 'payplug' ); ?>">
 					<?php wp_nonce_field( 'payplug_user_logout', '_logoutaction' ); ?>
-					|
-					<a href="https://portal.payplug.com" target="_blank"><?php _e( 'Go to my dashboard', 'payplug' ); ?></a>
-				</p>
-			</td>
-		</tr>
+                    |
+                    <a href="https://portal.payplug.com"
+                       target="_blank"><?php _e( 'Go to my dashboard', 'payplug' ); ?></a>
+                </p>
+            </td>
+        </tr>
 		<?php
 
 		return ob_get_clean();
