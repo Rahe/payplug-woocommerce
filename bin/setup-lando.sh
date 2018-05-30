8e72555341e7cb0968d296123096e2f00282cec6
Nicolas Juen <njuen@beapi.fr>
2018-05-31 00:17:02 +0200

    Create folder and copy once before the tests

--- bin/setup-lando.sh
+++ bin/setup-lando.sh
@@ -20,5 +20,10 @@ wp import sample_products.xml --authors=create
 # Storefront theme
 wp theme install --activate storefront
 
-# Make the folder for woocommerce
+# Make the folder for woocommerce, copy files initialy and folder
 mkdir wordpress/wp-content/plugins/payplug-woocommerce/
+cp payplug.php wordpress/wp-content/plugins/payplug-woocommerce/payplug.php
+cp -R src wordpress/wp-content/plugins/payplug-woocommerce/src/
+cp -R assets wordpress/wp-content/plugins/payplug-woocommerce/assets/
+cp -R languages wordpress/wp-content/plugins/payplug-woocommerce/languages/
+cp -R vendor wordpress/wp-content/plugins/payplug-woocommerce/vendor/
