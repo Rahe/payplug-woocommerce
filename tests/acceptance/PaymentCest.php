80b40418f5901223fe50080c7af123e56940cc3b
Nicolas Juen <njuen@beapi.fr>
2018-05-30 23:38:16 +0200

    Add the payment and payment cancel tests

--- tests/acceptance/PaymentCest.php
+++ tests/acceptance/PaymentCest.php
@@ -1,5 +1,5 @@
 <?php
-
+opcache_reset();
 
 class PaymentCest {
 	public function _before( AcceptanceTester $I ) {
@@ -24,15 +24,15 @@ class PaymentCest {
 		$I->wantToTest( 'In DEV mode, checkout displays message' );
 
 		// Message for testers
-		$I->see( 'Pay via PayPlug. You are in TEST MODE. In test mode you can use the card 4242424242424242 with any valid expiration date and CVC' );
+		$I->see( 'Pay via PayPlug. You are in TEST MODE. In test mode you can use the card 4242424242424242 with any valid expiration date and CVC.' );
 	}
 
 	/**
-	 * Fill the fields and test
+	 * Fill the form fields and test
 	 *
 	 * @param AcceptanceTester $I
 	 */
-	public function testNotConfiguredCartCheckout( AcceptanceTester $I ) {
+	public function testConfiguredCartCheckout( AcceptanceTester $I ) {
 		$I->wantToTest( 'In DEV mode and filled form, cart displays message' );
 
 		// Fill form
@@ -44,11 +44,57 @@ class PaymentCest {
 		$I->fillField( 'billing_email', "test@payplug.localhost" );
 		$I->fillField( 'billing_phone', "0123456789" );
 
+		// Check the options
+		$I->checkOption( 'payment_method' );
+
 		// wait ajax done, submit the form
 		$I->waitForElement( '#place_order' );
 		$I->click( '#place_order' );
 
+		// Wheck we are on Payplug page
+		$I->waitForText( 'VOTRE CARTE' );
+
 		// Right payment error
-		$I->waitForText( 'Error processing checkout. Please try again.' );
+		$I->fillField( [ 'id' => 'paymentCardNumber' ], "4242424242424242" );
+		$I->fillField( [ 'id' => 'paymentCardExpiration' ], "11/2099" );
+		$I->fillField( [ 'id' => 'paymentCardCvv' ], "123" );
+
+		$I->click( '#payButton' );
+
+		$I->waitForText( 'Order received' );
+		$I->waitForText( 'Thank you. Your order has been received.' );
+	}
+
+	/**
+	 * Fill the form fields and test
+	 *
+	 * @param AcceptanceTester $I
+	 */
+	public function testConfiguredCartCheckoutCancel( AcceptanceTester $I ) {
+		$I->wantToTest( 'In DEV mode and filled form, cancel the payplug' );
+
+		// Fill form
+		$I->fillField( 'billing_first_name', "First Name" );
+		$I->fillField( 'billing_last_name', "Last Name" );
+		$I->fillField( 'billing_address_1', "118 avenenue Jean Jaurès" );
+		$I->fillField( 'billing_city', "Paris" );
+		$I->fillField( 'billing_postcode', "75019" );
+		$I->fillField( 'billing_email', "test@payplug.localhost" );
+		$I->fillField( 'billing_phone', "0123456789" );
+
+		// Check the options
+		$I->checkOption( 'payment_method' );
+
+		// wait ajax done, submit the form
+		$I->waitForElement( '#place_order' );
+		$I->click( '#place_order' );
+
+		// Wheck we are on Payplug page
+		$I->waitForText( 'VOTRE CARTE' );
+
+		// Cancel
+		$I->click( '#linkBackMerchant' );
+
+		$I->waitForText( 'Your order was cancelled.' );
 	}
 }
