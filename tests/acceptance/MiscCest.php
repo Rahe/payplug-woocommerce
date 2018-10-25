7fda67e5c5800e46a55841fbb1a9c1c2e28e6fa1
Nicolas Juen <njuen@beapi.fr>
2018-10-25 10:23:25 +0200

    General tests for the platform

---
+++ tests/acceptance/MiscCest.php
@@ -0,0 +1,22 @@
+<?php
+
+class MiscCest {
+	/**
+	 * Admin pages as woocommerce deactivated
+	 *
+	 * @param AcceptanceTester $I
+	 */
+	public function testNoticeWoocommerce( AcceptanceTester $I ) {
+		$I->wantToTest( 'I want to test there is an notice if plugin activated but no Woocommerce' );
+
+		// As admin
+		$I->loginAsAdmin();
+
+		// Deactivate the plugin
+		$I->amOnPluginsPage();
+		$I->deactivatePlugin('woocommerce');
+
+		// Notice to activate Woocommerce
+		$I->see( 'PayPlug requires an active version of WooCommerce' );
+	}
+}
