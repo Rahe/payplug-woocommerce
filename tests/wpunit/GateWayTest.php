c3cd55c9ede538b1b6519f9668985ffbd1eac175
Nicolas Juen <njuen@beapi.fr>
2018-11-23 14:27:52 +0100

    Add comments on tests

--- tests/wpunit/GateWayTest.php
+++ tests/wpunit/GateWayTest.php
@@ -20,7 +20,7 @@ class GateWayTest extends \Codeception\TestCase\WPTestCase {
 	public function testGetUserApiKeysMissingEmail() {
 		$gateway = new \Payplug\PayplugWoocommerce\Gateway\PayplugGateway();
 
-		$this->assertWPError( $gateway->retrieve_user_api_keys( '', 'ele' ), 'LOl' );
+		$this->assertWPError( $gateway->retrieve_user_api_keys( '', 'ele' ) );
 	}
 
 	public function testGetUserApiKeysMissingPassWord() {
@@ -44,38 +44,53 @@ class GateWayTest extends \Codeception\TestCase\WPTestCase {
 	public function testLimitStringLengthDefault() {
 		$gateway = new \Payplug\PayplugWoocommerce\Gateway\PayplugGateway();
 
-		$this->assertEquals( 4, strlen( $gateway->limit_length( "TEST" ) ) );
+		// Under string limit, should not change.
+		$this->assertEquals( 4, strlen( $gateway->limit_length( 'TEST' ) ) );
 
-		$this->assertEquals( 100, strlen( $gateway->limit_length( "A44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtL" ) ) );
+		// At string limit, should not change.
+		$this->assertEquals( 100, strlen( $gateway->limit_length( 'A44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtL' ) ) );
 
-		$this->assertEquals( 100, strlen( $gateway->limit_length( "ADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtL" ) ) );
+		// Above string limit, should equal to default limit.
+		$this->assertEquals( 100, strlen( $gateway->limit_length( 'ADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtL' ) ) );
 	}
 
 	public function testLimitStringLength255() {
 		$gateway = new \Payplug\PayplugWoocommerce\Gateway\PayplugGateway();
 
-		$this->assertEquals( 4, strlen( $gateway->limit_length( "TEST", 255 ) ) );
+		// Under string limit, should not change.
+		$this->assertEquals( 4, strlen( $gateway->limit_length( 'TEST', 255 ) ) );
 
-		$this->assertEquals( 100, strlen( $gateway->limit_length( "A44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtL", 255 ) ) );
+		// Under string limit, should not change.
+		$this->assertEquals( 100, strlen( $gateway->limit_length( 'A44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtL', 255 ) ) );
 
-		$this->assertEquals( 255, strlen( $gateway->limit_length( "ADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QAAAAAA", 255 ) ) );
+		// At string limit, should not change.
+		$this->assertEquals( 255, strlen( $gateway->limit_length( 'ADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QAAAAAA', 255 ) ) );
 
-		$this->assertEquals( 255, strlen( $gateway->limit_length( "ADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QAAAAAADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QAAAAA", 255 ) ) );
+		// Above string limit, should equal to passed limit.
+		$this->assertEquals( 255, strlen( $gateway->limit_length( 'ADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QAAAAAADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QvzH4pMkEuZ408CBIECZIb7L5zVZQEN1uonf4srt4UZ293EPrs6Acr0giEGInMSjGtkzI5TFpIJccuQjZcrchsi3OlFZNKXtLADSFDSFDSFSDFSFA44QAAAAA', 255 ) ) );
 
 	}
 
 	public function testValidateOrderAmountOutOfRange() {
 		$gateway = new \Payplug\PayplugWoocommerce\Gateway\PayplugGateway();
 
+		// Under the limit.
 		$this->assertWPError( $gateway->validate_order_amount( 50 ) );
+
+		// Above the limit.
 		$this->assertWPError( $gateway->validate_order_amount( 2000050 ) );
 	}
 
 	public function testValidateOrderAmountInRange() {
 		$gateway = new \Payplug\PayplugWoocommerce\Gateway\PayplugGateway();
 
+		// At valid minimum amount.
 		$this->assertEquals( 99, $gateway->validate_order_amount( 99 ) );
+
+		// At valid maximum amount.
 		$this->assertEquals( 2000000, $gateway->validate_order_amount( 2000000 ) );
+
+		// Into limits valid limits.
 		$this->assertEquals( 500, $gateway->validate_order_amount( 500 ) );
 	}
 
