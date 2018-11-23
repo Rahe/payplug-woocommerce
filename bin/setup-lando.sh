7073e59ddaeca2530eb0c1ef3319277fa7238ab4
Nicolas Juen <njuen@beapi.fr>
2018-11-23 15:42:15 +0100

    Fix the script for the boot of the tests

--- bin/setup-lando.sh
+++ bin/setup-lando.sh
@@ -1,42 +1,129 @@
 #!/usr/bin/env bash
 
-WOOCOMMERCE_VERSION=${1-3.5.0}
-WP_VERSION=${2-latest}
+# Variables
+WOOCOMMERCE_VERSION=${WOOCOMMERCE_VERSION-""}
+WP_VERSION="${WP_VERSION:-latest}"
+WP_LOCALE="${WP_LOCALE:-fr_FR}"
+PHP_VERSION="${PHP_VERSION:-7.0}"
 
-composer install --prefer-dist -o
-mkdir -p wordpress
-cd wordpress
-wp db reset --yes
-wp core download --version=${WP_VERSION} --force
-wp config create --dbname="wordpress" --dbuser="wordpress" --dbpass="wordpress" --dbhost="database" --dbprefix="wp_"
-wp core install --url="https://payplug.lndo.site" --title="Test" --admin_user="admin" --admin_password=admin --admin_email="admin@payplug.lndo.site" --skip-email
-wp rewrite structure '/%postname%/' --hard
+# Get to the root of the plugin folder
+cd "$(dirname "$0")"
+cd ..
 
+# Hack - awaiting https://github.com/lando/lando/pull/750
+perl -pi -we "s/^  php: .*/  php: '$PHP_VERSION'/" .lando.yml
+
+lando start -v
+#lando wp --version || lando bash test/install-wp-cli.sh
+
+# Remove previous version of WP
+rm -rf wordpress/[a-z]*
+
+# Download WP into the right version and locale
+lando wp core download \
+    --path=/app/wordpress/ \
+    --version=$WP_VERSION \
+    --locale=$WP_LOCALE
+
+# Create the wp-config.php file
+lando wp config create \
+    --path=/app/wordpress/ \
+    --dbname=wordpress \
+    --dbuser=wordpress \
+    --dbpass=wordpress \
+    --dbhost=database
+
+# Deactive the updates
+lando wp config set \
+    --path=/app/wordpress/ \
+    --type=constant \
+    --raw \
+    WP_AUTO_UPDATE_CORE false
+
+# Activate the DEBUG
+lando wp config set \
+    --path=/app/wordpress/ \
+    --type=constant \
+    --raw \
+    WP_DEBUG true
+
+# Reset the database
+lando wp db reset \
+    --path=/app/wordpress/ \
+    --yes
+
+# Create our instance
+wp_url="https://payplug.lndo.site"
+lando wp core install \
+    --path=/app/wordpress/ \
+    --url="$wp_url" \
+    '--title="My Test Site"' \
+    --admin_user="admin" \
+    --admin_password="admin" \
+    --admin_email="admin@payplug.lndo.site" \
+    --skip-email
+
+# Setup permalinks and create the .htaccess file
+lando wp rewrite structure \
+    --path=/app/wordpress/ \
+    '/%postname%/' \
+    --hard
 
 # Install Woocommerce to needed version
-wp plugin install --activate woocommerce --force --version=${WOOCOMMERCE_VERSION}
-wp plugin install --activate wordpress-importer
+lando wp plugin install \
+    --activate woocommerce \
+    --force \
+    --version=${WOOCOMMERCE_VERSION} \
+    --path=/app/wordpress/
+
+# Add importer
+lando wp plugin install \
+    --activate \
+    wordpress-importer \
+    --path=/app/wordpress/
+
+# Get the product sample
 wget https://raw.githubusercontent.com/woocommerce/woocommerce/master/sample-data/sample_products.xml
 
 # Import the products
-wp import sample_products.xml --authors=create
+lando wp import \
+    sample_products.xml \
+    --authors=create \
+    --path=/app/wordpress/
+
+# Remove the useless file
 rm sample_products.xml
 
-# Storefront theme
-wp theme install --activate storefront
+# Add storefront theme and activate
+lando wp theme install \
+    --activate storefront \
+    --path=/app/wordpress/
 
-# Make the folder for woocommerce, copy files initialy and folder
-cd ..
-mkdir -p wordpress/wp-content/plugins/payplug-woocommerce/
-cp -R payplug.php wordpress/wp-content/plugins/payplug-woocommerce/
-cp -R composer.json wordpress/wp-content/plugins/payplug-woocommerce/
-cp -R composer.lock wordpress/wp-content/plugins/payplug-woocommerce/
-cp -R src wordpress/wp-content/plugins/payplug-woocommerce/
-cp -R assets wordpress/wp-content/plugins/payplug-woocommerce/
-cp -R languages wordpress/wp-content/plugins/payplug-woocommerce/
-cd wordpress/wp-content/plugins/payplug-woocommerce/
-composer install --no-dev --prefer-dist -o
+# Make the folder for plugin, copy files initially and folder
+PLUGIN_FOLDER="wordpress/wp-content/plugins/payplug-woocommerce/"
+mkdir -p $PLUGIN_FOLDER
+cp -R payplug.php $PLUGIN_FOLDER
+cp -R composer.json $PLUGIN_FOLDER
+cp -R composer.lock $PLUGIN_FOLDER
+cp -R src $PLUGIN_FOLDER
+cp -R assets $PLUGIN_FOLDER
+cp -R languages $PLUGIN_FOLDER
+cd $PLUGIN_FOLDER
+
+lando composer install --no-dev --prefer-dist -o
 
 # Activate plugin
 cd ../../../
-wp plugin activate payplug-woocommerce
+lando wp plugin activate \
+    payplug-woocommerce
+
+echo '✅ Everything installed ! ✅'
+echo 'You have the commands :
+ - `lando test-acceptance` for launching one test
+ - `test-acceptance-all` for launching all acceptance tests
+ - `test-wpunit` for launching one wordpress unit test
+ - `test-wpunit-all` for launching all wordpress unit tests
+
+All the results will be displayed on tests/_output/ if any error occurs.
+'
+echo 'For all the lando commands just type `lando`'
